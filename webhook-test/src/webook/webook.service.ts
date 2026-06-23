289dbbd29c7dd492d6cc49f7eb50bef29d247819
jrweqlop <herher.haha@windowslive.com>
2026-06-23 13:51:32 +0700

    update
    update

--- webhook-test/src/webook/webook.service.ts
+++ webhook-test/src/webook/webook.service.ts
@@ -20,9 +20,19 @@ export class WebookService {
     // return 'This action adds a new webook';
     // return result
     try {
+      const url = {
+        dev: 'http://localhost:4000/api/messaging/webhook',
+        main: 'https://mhssw-srih.moph.go.th/webhook/api/messaging/webhook'
+      }
       // const result = await firstValueFrom(this.httpService.post('http://localhost:4000/api/messaging/webhook', data))
-      const result = await firstValueFrom(this.httpService.post('https://mhssw-srih.moph.go.th/webhook/api/messaging/webhook', data))
-      return result.data
+      // const resultx = await firstValueFrom(this.httpService.post(url.dev, data))
+      // return resultx.data
+      const result = this.httpService.axiosRef.post(url.main, data, {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      }).then((res) => res.data).catch(() => { return { code: 400, message: 'cannot', data: false } })
+      return result
     } catch (e) {
       return { code: 400, message: 'cannot', data: false }
     }
