c36782dc3702eec87a4ab94c55ef5c2150d1b6ff
jrweqlop <herher.haha@windowslive.com>
2026-06-23 11:24:53 +0700

    update
    enable cors

--- webhook-test/src/main.ts
+++ webhook-test/src/main.ts
@@ -2,10 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const port = process.env.PORT ?? 3000
   await app.listen(port, '0.0.0.0', () => {
-    console.log('start port : ',port)
+    console.log('start port : ', port)
   });
 }
 bootstrap();
